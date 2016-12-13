d8162986112f0f6ae6d161902c02d565cfbf293e
Smolkin <smolkin.sergey@gmail.com>
2016-12-13 21:18:17 +0300

    Первый метод

---
+++ TestService/UgzFssService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FSS_UGZ_Service
+{
+    public class UgzFssService : IUgzFssService
+    {
+        public ElectronicInterchangeType SendNotificationChanges(NotificationChangesType ContractPayments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProcessingResultsType NotificationChangesResult(ElectronicInterchangeType ElectronicInterchange)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
